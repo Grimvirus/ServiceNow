6f188a09bcc863315c8d8fffb9a2315effee8542
Grimvirus <fruitloop@gmx.net>
2016-07-07 19:23:47 +0200

    Create PDP Creator.js

---
+++ HIL/PDP Creator.js
@@ -0,0 +1,55 @@
+// DD.MM.YYYY - Author - Comments 
+// 06.07.2016 - OD - Execute PDP Creator file 
+
+try { 
+(function(){ 
+var current = new GlideRecord("sc_req_item"); 
+current.get("c320e1884f246600198d119f0310c76d"); 
+
+var parameters = []; 
+// PDPUserCreator.exe "INC001234" "C:\Temp" "Raymond Eberle" "EBERRAY" "Switzerland" "P" "Product Manager Maintain" "Technical Developer" 
+
+parameters.push(String(current.number)); // RITM 
+parameters.push(String(gs.getProperty("com.hilti.pdp.target"))); 
+parameters.push(String(current.variables.required_for.name)); 
+parameters.push(String(current.variables.required_for.user_name)); 
+parameters.push(String(current.variables.location.getDisplayValue())); 
+if (current.variables.system == "Production") { 
+parameters.push(String("P")); 
+} else { 
+parameters.push(String("P")); 
+} 
+
+// Get Privleges 
+var gr = new GlideRecord("item_option_new"); 
+gr.addEncodedQuery("variable_set=efa645c670a23100c4c8c7a839dee1d3^GOTOnameLIKEchk_^active=true"); 
+gr.query(); 
+while (gr.next()) { 
+if (current.variables[gr.name] == "true" || current.variables[gr.name] == true) { 
+parameters.push(String(gr.question_text)); 
+} 
+} 
+
+var fn = ""; 
+fn = gs.getProperty("com.hilti.pdp.filepath"); 
+
+if (!fn) { 
+return; 
+} 
+
+var mid = new GlideRecord("ecc_agent"); 
+mid.addQuery("status", "Up"); 
+mid.query(); 
+if (mid.next()) { 
+var commandprobe1 = new CommandProbeES(String(mid.name)); 
+commandprobe1.addParameter("skip_sensor", "true"); 
+commandprobe1.setCommand(fn + ' \"' + parameters.join('\" \"') + '\"'); 
+gs.log(commandprobe1.create()); 
+return; 
+} else { 
+return; 
+} 
+})(); 
+} catch(err) { 
+gs.eventQueue("script.error", current, "ERROR: Execute PDPUserCreator", err + " in line:" + err.lineNumber); 
+}
