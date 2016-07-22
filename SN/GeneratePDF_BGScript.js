dbe9b3c633a0b112188fb2f40d430f0939102005
Grimvirus <fruitloop@gmx.net>
2016-07-22 07:38:05 +0200

    Update and rename GeneratePDF.js to GeneratePDF_BGScript.js

---
+++ SN/GeneratePDF_BGScript.js
@@ -0,0 +1,3 @@
+var html = '<p align="center"><font size="2"> </font></p> <p><font size="2">{{Date}}</font></p> <p><font size="2"><strong style="font-size: small;">COMPANY, Inc</strong><span style="font-size: small;">.<br /></span><br /></font></p> <p><font size="2">COMPANY ADDRESS</font>'; 
+
+PFGeneralForm.generate("incident", "e8caedcbc0a80164017df472f39eaed1", "incident", "e8caedcbc0a80164017df472f39eaed1", "od.pdf", html);
