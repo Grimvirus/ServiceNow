c9d1081d4ac526a68cbd7c285bcb02d023529cd1
Grimvirus <fruitloop@gmx.net>
2016-12-08 17:47:31 +0100

    Create ResizerHack.js

---
+++ ResizerHack.js
@@ -0,0 +1,16 @@
+//var time = 300;
+		
+		/*var interval = setInterval(function() {
+			jQuery("div.no-gutter-left").resize();
+		}, time);*/
+
+
+		/*jQuery("div.no-gutter-left").resize(function(){
+			var lHtotal = 0;
+			var rHtotal = 0;
+			jQuery("div.no-gutter-left > span").each(function(){rHtotal += jQuery(this).children("div").height();});
+			jQuery("div.no-gutter-right > span").each(function(){lHtotal += jQuery(this).children("div").height();});
+			if (rHtotal < lHtotal) { rHtotal = lHtotal; }
+			jQuery("div.no-gutter-right").css({"height":rHtotal+"px","background-color":"#3869FA","opacity":"0.95","background-clip":"content-box"});
+			jQuery("div.no-gutter-left").css({"height":rHtotal+"px","background-color":"white","opacity":"0.95","background-clip":"content-box"});
+		});*/
