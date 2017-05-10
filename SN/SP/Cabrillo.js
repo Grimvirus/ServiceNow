49fdb7bcb76be1c12f229e434d8d83f328749453
Grimvirus <fruitloop@gmx.net>
2017-05-10 19:29:06 +0200

    Create Cabrillo.js

---
+++ SN/SP/Cabrillo.js
@@ -0,0 +1,23 @@
+Log Object
+Object
+EXPORT_NAME: string = snmCabrillo
+attachments: Object
+debug: Object
+auth: Object
+viewLayout: Object
+MORE_MENU_BUTTON_STYLE: string = moreMenuButtonStyle
+SUCCESS_SPINNER_STYLE: string = successStyle
+list: Object
+DEBUG: boolean = true
+CLIENT_EXPORT_NAME: string = CabrilloClient
+navigation: Object
+form: Object
+PACKAGE: string = com.servicenow.cabrillo
+camera: Object
+modal: Object
+MODAL_PRESENTATION_STYLE_FORM_SHEET: string = formSheet
+CLOSE_BUTTON_STYLE_DONE: string = done
+CLOSE_BUTTON_STYLE_CLOSE: string = close
+MODAL_PRESENTATION_STYLE_FULL_SCREEN: string = fullScreen
+CLOSE_BUTTON_STYLE_CANCEL: string = cancel
+geolocation: Object
